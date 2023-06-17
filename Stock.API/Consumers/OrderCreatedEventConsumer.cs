7d7802e478d82ab6f09a105a461c1f96b54b4e43
Samed Kütahyalı <smdkthyli@gmail.com>
2023-06-17 21:10:21 +0300

    Stock.Api has been created.

---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,70 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using Stock.API.Models;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<OrderCreatedEventConsumer> _logger;
+        private readonly ISendEndpointProvider _sendEndpointProvider;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public OrderCreatedEventConsumer(AppDbContext context, ILogger<OrderCreatedEventConsumer> logger, ISendEndpointProvider sendEndpointProvider, IPublishEndpoint publishEndpoint)
+        {
+            _context = context;
+            _logger = logger;
+            _sendEndpointProvider = sendEndpointProvider;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            var stockResult = new List<bool>();
+
+            foreach (var item in context.Message.orderItems)
+            {
+                stockResult.Add(await _context.Stocks.AnyAsync(x => x.ProductId == item.ProductId && x.Count > item.Count));
+            }
+
+            if (stockResult.All(x => x.Equals(true))) //Stock listesindeki ürünlerin hepsinin stoğu var ise == true
+            {
+                foreach (var item in context.Message.orderItems)
+                {
+                    var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.ProductId == item.ProductId); 
+
+                    if (stock is not null)
+                        stock.Count -= item.Count;  //Ürünleri stoktan düş
+
+                    await _context.SaveChangesAsync();
+                }
+
+                _logger.LogInformation($"Stock was reserved for Buyer Id: {context.Message.BuyerId}");
+
+                var sendEndpoint = await _sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettingsConst.StockReservedEventQueueName}")); //Send ile bir kuyruğa gönderdik. Publish yapsaydık buna gerek yoktu!
+
+                StockReservedEvent stockReservedEvent = new()
+                {
+                    Payment = context.Message.Payment,
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    OrderItems = context.Message.orderItems
+                };
+
+                await sendEndpoint.Send(stockReservedEvent);
+            }
+            else
+            {
+                await _publishEndpoint.Publish(new StockNotReservedEvent
+                {
+                    OrderId = context.Message.OrderId,
+                    Message = "Not enough stock!"
+                });
+
+                _logger.LogInformation($"Not enough stock reserved for Buyer Id: {context.Message.BuyerId}");
+            }
+        }
+    }
+}
--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,13 +1,58 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using Stock.API.Consumers;
+using Stock.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<OrderCreatedEventConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+
+        cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockOrderCreatedEventQueueName, e =>
+        {
+            e.ConfigureConsumer<OrderCreatedEventConsumer>(context);
+        });
+    });
+});
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("StockDb");
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Stocks.Add(new Stock.API.Models.Stock
+    {
+        Id = 1,
+        ProductId = 1,
+        Count = 100
+    });
+    context.Stocks.Add(new Stock.API.Models.Stock
+    {
+        Id = 2,
+        ProductId = 2,
+        Count = 200
+    });
+    context.SaveChanges();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
