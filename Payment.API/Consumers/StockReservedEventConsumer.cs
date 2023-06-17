fc10f13c873d28f217955d8fce965e0678a5ee5a
Samed Kütahyalı <smdkthyli@gmail.com>
2023-06-17 22:16:00 +0300

    Payment.Api has been created.

---
+++ Payment.API/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Shared;
+
+namespace Payment.API.Consumers
+{
+    public class StockReservedEventConsumer : IConsumer<StockReservedEvent>
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<StockReservedEventConsumer> _logger;
+
+        public StockReservedEventConsumer(ILogger<StockReservedEventConsumer> logger, IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            var balance = 3000m;
+
+            if (balance > context.Message.Payment.TotalPrice)
+            {
+                _logger.LogInformation($" {context.Message.Payment.TotalPrice} TL was withdrawn from credit card for user id = {context.Message.BuyerId}");
+
+                await _publishEndpoint.Publish(new PaymentSucceededEvent
+                {
+                    OrderId = context.Message.OrderId,
+                    BuyerId = context.Message.BuyerId
+                });
+            }
+            else
+            {
+                _logger.LogInformation($" {context.Message.Payment.TotalPrice} TL was not withdrawn from credit card for user id = {context.Message.BuyerId}");
+
+                await _publishEndpoint.Publish(new PaymentFailedEvent
+                {
+                    OrderId = context.Message.OrderId,
+                    BuyerId = context.Message.BuyerId,
+                    Message = "Not enough balance!"
+                });
+            }
+        }
+    }
+}
